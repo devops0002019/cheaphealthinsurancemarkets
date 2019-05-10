1e0d54eec159f1902f252cd935b707c9a6d2fe36
Churchill Exotics <churchillexotics@iMac.local>
2019-05-10 11:23:40 -0400

    add a new file

--- js/styles.js
+++ js/styles.js
@@ -1,16 +1,17 @@
+
+
 document.documentElement.style .setProperty('--primary', '#74BDCB');
 document.documentElement.style .setProperty('--secondary', '#262626');
 document.documentElement.style .setProperty('--tertiary', '#FAFAFA');
 document.documentElement.style .setProperty('--quaternary', '#0292B7');
 document.documentElement.style .setProperty('--border', '#d8d8d8');
+
 document.documentElement.style .setProperty('--page-max-width', '1366px');
-document.documentElement.style .setProperty('--navbar-background', 'transparent');
-document.documentElement.style .setProperty('--navbar-text-color', '#0292B7');
+
+document.documentElement.style .setProperty('--navbar-background', 'var(--primary)');
+document.documentElement.style .setProperty('--navbar-text-color', 'var(--secondary)');
 document.documentElement.style .setProperty('--header-background', 'white');
-document.documentElement.style .setProperty('--header-text-color', 'none');
-document.documentElement.style .setProperty('--footer-background', 'white');
-document.documentElement.style .setProperty('--footer-text-color', 'black');
-document.documentElement.style .setProperty('--h1-color', 'black');
-document.documentElement.style .setProperty('--h2-color', '#000');
-document.documentElement.style .setProperty('--h3-color', '#262626');
-document.documentElement.style .setProperty('--seo-page-title-color', '#fff');
+document.documentElement.style .setProperty('--header-text-color', 'blue');
+document.documentElement.style .setProperty('--footer-background', 'var(--primary)');
+document.documentElement.style .setProperty('--footer-text-color', 'var(--secondary)');
+
