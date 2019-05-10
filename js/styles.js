409000161c523720d702f32265495d8397f49039
Churchill Exotics <churchillexotics@iMac.local>
2019-05-10 12:19:41 -0400

    add a new file

--- js/styles.js
+++ js/styles.js
@@ -1,7 +1,7 @@
 
 
-document.documentElement.style .setProperty('--primary', '#74BDCB');
-document.documentElement.style .setProperty('--secondary', '#262626');
+document.documentElement.style .setProperty('--primary', '#633e5a');
+document.documentElement.style .setProperty('--secondary', '#fff');
 document.documentElement.style .setProperty('--tertiary', '#FAFAFA');
 document.documentElement.style .setProperty('--quaternary', '#0292B7');
 document.documentElement.style .setProperty('--border', '#d8d8d8');
@@ -10,7 +10,7 @@ document.documentElement.style .setProperty('--page-max-width', '1366px');
 
 document.documentElement.style .setProperty('--navbar-background', 'var(--primary)');
 document.documentElement.style .setProperty('--navbar-text-color', 'var(--secondary)');
-document.documentElement.style .setProperty('--header-background', 'white');
+document.documentElement.style .setProperty('--header-background', '');
 document.documentElement.style .setProperty('--header-text-color', 'blue');
 document.documentElement.style .setProperty('--footer-background', 'var(--primary)');
 document.documentElement.style .setProperty('--footer-text-color', 'var(--secondary)');
